9aa0ed5a195e5204ee25bab4f845f9bc23044f3a
Wanasit Tanakitrungruang <berrysboy@gmail.com>
2016-05-21 13:14:31 +0900

    Change the basic model to KNN

--- basic_model.py
+++ basic_model.py
@@ -1,23 +1,25 @@
 import pandas as pd
 import numpy as np
 
-train_data = pd.read_csv('./train.csv')
+train_data = pd.read_csv('../input/train.csv')
 
 def ll(text):
     return int(float(text)*10000)
 
-train_data['NormLatitude'] = train_data['Latitude'].apply(ll)
-train_data['NormLongitude'] = train_data['Longitude'].apply(ll)
-
-X = train_data[['NormLatitude', 'NormLongitude']]
+X = train_data[['Latitude', 'Longitude']]
 y = train_data['WnvPresent']
 
 
 X_train = X[:len(X)/2]
 y_train = y[:len(X)/2]
+
 X_valid = X[len(X)/2:]
 y_valid = y[len(X)/2:]
 
-from sklearn import linear_model
-clf = linear_model.LinearRegression()
-clf.fit(X_train, y_train)
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier()
+knn.fit(X_train, y_train)
+
+
+print knn.score(X_valid, y_valid)
